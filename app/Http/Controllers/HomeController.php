eb83df25825513f6b06dc28be7898e265a6c6ee7
obaidul-islam <obaidul15-2255@diu.edu.bd>
2023-01-31 10:08:15 +0600

    Display doctor data in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Doctor;
 
 class HomeController extends Controller
 {
@@ -15,7 +16,8 @@ class HomeController extends Controller
         if(Auth::id()){
 
             if(Auth::user()->usertype=='0'){
-                return view('user.home');
+                $doctor= doctor::all();
+                return view('user.home',compact('doctor'));
             }else{
                 return view('admin.home');
             }
@@ -29,7 +31,12 @@ class HomeController extends Controller
 
     public function index(){
 
-        return view('user.home');
+        if(Auth::id()){
+            return redirect('home');
+        }else{ 
+        $doctor= doctor::all();
+        return view('user.home',compact('doctor'));
+        }
     }
 
 
